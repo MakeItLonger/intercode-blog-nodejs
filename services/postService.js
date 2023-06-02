5c8f8f41f044ae8064b94b10cd1ae0aa397dec7c
MakeItLonger <zavadiuk@gmail.com>
2023-06-02 14:25:31 +0300

    refactor from filtering, sorting and pagination from chaining to aggregation

--- services/postService.js
+++ services/postService.js
@@ -12,6 +12,51 @@ class PostService {
     return createdPost;
   }
 
+  // async getAllPosts(queryParams) {
+  //   const now = new Date();
+  //   const startOfYear = new Date(now.getFullYear(), 0, 1);
+
+  //   const page = parseInt(queryParams.page) - 1 || 0;
+  //   const limit = parseInt(queryParams.limit) || 5;
+  //   const search = queryParams.search || '';
+  //   const dateStart = queryParams.datestart || startOfYear;
+  //   const dateEnd = queryParams.dateend || String(Date.now());
+  //   let sort = queryParams.sort || 'title';
+  //   let topic = queryParams.topic || '';
+
+  //   const topicOptions = ['Life style', 'Home', 'Hobby', 'Travel'];
+
+  //   topic === '' ? (topic = [...topicOptions]) : (topic = queryParams.topic.split(','));
+  //   queryParams.sort ? (sort = queryParams.sort.split(',')) : (sort = [sort]);
+
+  //   let sortBy = {};
+  //   if (sort[1]) {
+  //     sortBy[sort[0]] = sort[1];
+  //   } else {
+  //     sortBy[sort[0]] = 'asc';
+  //   }
+
+  //   const posts = await postModel
+  //     .find({ title: { $regex: search, $options: 'i' } })
+  //     .where('topic')
+  //     .in([...topic])
+  //     .where('date')
+  //     .gte(dateStart)
+  //     .where('date')
+  //     .lte(dateEnd)
+  //     .sort(sortBy)
+  //     .skip(page * limit)
+  //     .limit(limit);
+
+  //   const total = await postModel.countDocuments({
+  //     title: { $regex: search, $options: 'i' },
+  //     topic: { $in: [...topic] },
+  //     date: { $gte: dateStart, $lte: dateEnd },
+  //   });
+
+  //   return { posts, total };
+  // }
+
   async getAllPosts(queryParams) {
     const now = new Date();
     const startOfYear = new Date(now.getFullYear(), 0, 1);
@@ -19,40 +64,34 @@ class PostService {
     const page = parseInt(queryParams.page) - 1 || 0;
     const limit = parseInt(queryParams.limit) || 5;
     const search = queryParams.search || '';
-    const dateStart = queryParams.datestart || startOfYear;
-    const dateEnd = queryParams.dateend || String(Date.now());
-    let sort = queryParams.sort || 'title';
-    let topic = queryParams.topic || '';
+    const dateStart = new Date(queryParams.datestart) || startOfYear;
+    const dateEnd = new Date(queryParams.dateend) || now;
+    const sort = queryParams.sort || 'title';
+    const topic = queryParams.topic ? queryParams.topic.split(',') : ['Life style', 'Home', 'Hobby', 'Travel'];
 
-    const topicOptions = ['Life style', 'Home', 'Hobby', 'Travel'];
+    const matchStage = {
+      title: { $regex: search, $options: 'i' },
+      topic: { $in: topic },
+      date: { $gte: dateStart, $lte: dateEnd },
+    };
 
-    topic === '' ? (topic = [...topicOptions]) : (topic = queryParams.topic.split(','));
-    queryParams.sort ? (sort = queryParams.sort.split(',')) : (sort = [sort]);
-
-    let sortBy = {};
-    if (sort[1]) {
-      sortBy[sort[0]] = sort[1];
+    const sortStage = {};
+    if (sort.includes(',')) {
+      const [sortField, sortOrder] = sort.split(',');
+      sortStage[sortField] = sortOrder === 'desc' ? -1 : 1;
     } else {
-      sortBy[sort[0]] = 'asc';
+      sortStage[sort] = 1;
     }
 
-    const posts = await postModel
-      .find({ title: { $regex: search, $options: 'i' } })
-      .where('topic')
-      .in([...topic])
-      .where('date')
-      .gte(dateStart)
-      .where('date')
-      .lte(dateEnd)
-      .sort(sortBy)
-      .skip(page * limit)
-      .limit(limit);
+    const skipStage = { $skip: page * limit };
+    const limitStage = { $limit: limit };
 
-    const total = await postModel.countDocuments({
-      title: { $regex: search, $options: 'i' },
-      topic: { $in: [...topic] },
-      date: { $gte: dateStart, $lte: dateEnd },
-    });
+    const aggregationPipeline = [{ $match: matchStage }, { $sort: sortStage }, skipStage, limitStage];
+
+    const [posts, total] = await Promise.all([
+      postModel.aggregate(aggregationPipeline),
+      postModel.countDocuments(matchStage),
+    ]);
 
     return { posts, total };
   }
